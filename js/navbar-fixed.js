4bf223581d2a14b08f62f8c71cfcb5b4c1cabaac
DevABID <92073265+DevABID@users.noreply.github.com>
2024-06-13 21:35:31 +0600

    Add files via upload

---
+++ js/navbar-fixed.js
@@ -0,0 +1,15 @@
+//slick start
+$('.slider').slick({
+	infinte: true,
+	slideToShow: 1,
+	slideToScroll: 1
+});
+//carousel start
+$('.carousel').carousel({
+	interval : 3000,
+	pause: 'hover',
+	wrap: true,
+	keyboard: true
+  });
+  //wow js start
+  new WOW().init();
